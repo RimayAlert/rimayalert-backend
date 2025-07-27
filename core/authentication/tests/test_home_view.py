da62879575d9e043545ce9738b90db74f7a5b8e1
FreddyAndres <fzambranoq2@unemi.edu.ec>
2025-07-27 13:08:08 -0500

    refactor(Test): :wrench:  update home view test to check login template

--- core/authentication/tests/test_home_view.py
+++ core/authentication/tests/test_home_view.py
@@ -4,6 +4,6 @@ from django.urls import reverse
 
 class HomeViewTests(TestCase):
     def test_home_view_renders_correctly(self):
-        response = self.client.get(reverse('home'))  # Asegúrate de que esa ruta existe
+        response = self.client.get(reverse('login'))  # Asegúrate de que esa ruta existe
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'home/home.html')
+        self.assertTemplateUsed(response, 'authentication/login.html')
