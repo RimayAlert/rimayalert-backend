fbe94c93aa970a754ad847340f74b550b613cc70
FreddyAndres <fzambranoq2@unemi.edu.ec>
2025-08-10 20:20:41 -0500

    refactor(Test): :wrench: update home view test to use correct template path for login

--- core/authentication/tests/test_home_view.py
+++ core/authentication/tests/test_home_view.py
@@ -5,5 +5,5 @@ class HomeViewTests(TestCase):
     def test_home_view_renders_correctly(self):
         response = self.client.get(reverse('authentication:login'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'authentication/login.html')
+        self.assertTemplateUsed(response, 'login/login.html')
 
