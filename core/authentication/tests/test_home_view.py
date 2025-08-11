9032f38f462740a58c301943c188eb19dea82614
FreddyAndres <fzambranoq2@unemi.edu.ec>
2025-08-10 20:17:49 -0500

    refactor(Test): :wrench: update home view test to use namespaced URL for login

--- core/authentication/tests/test_home_view.py
+++ core/authentication/tests/test_home_view.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 from django.urls import reverse
 
-
 class HomeViewTests(TestCase):
     def test_home_view_renders_correctly(self):
-        response = self.client.get(reverse('login'))
+        response = self.client.get(reverse('authentication:login'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'authentication/login.html')
+
