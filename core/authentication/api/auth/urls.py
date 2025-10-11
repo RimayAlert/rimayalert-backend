4b1b9f92878ce2d27c286df95bca522c17e8c952
fzambranoq2 <fzambranoq2@unemi.edu.ec>
2025-10-11 05:05:41 -0500

    feat(auth): :sparkle: implement custom authentication token API and user creation logic

--- core/authentication/api/auth/urls.py
+++ core/authentication/api/auth/urls.py
@@ -1,4 +1,7 @@
-from django.urls import path, include
+from django.urls import path
+
+from core.authentication.api.auth.views.auth import CustomAuthTokenApiView
 
 urlpatterns = [
+    path('-token', CustomAuthTokenApiView.as_view(), name='api_token_auth'),
 ]
---
+++ core/authentication/api/auth/views/auth.py
@@ -0,0 +1,29 @@
+import logging
+
+from django.db import transaction
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from core.authentication.api.auth.features.user import AuthApiUser
+from core.authentication.api.auth.serializers.user import AuthTokenSerializerInput
+
+logger = logging.getLogger(__name__)
+
+
+class CustomAuthTokenApiView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        try:
+            with transaction.atomic():
+                serializer = AuthTokenSerializerInput(data=request.data)
+                if serializer.is_valid(raise_exception=True):
+                    auth_user = AuthApiUser(self.request)
+                    result = auth_user.login(serializer.validated_data)
+                    token = result['token']
+                    return Response({'token': token}, status.HTTP_200_OK)
+        except Exception as e:
+            logger.error(str(e))
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
