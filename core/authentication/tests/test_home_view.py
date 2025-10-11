304b4f5e19975ba163285759e8105de0c02ea1b9
fzambranoq2 <fzambranoq2@unemi.edu.ec>
2025-10-11 06:09:55 -0500

    test(auth): :rotating_light: update authentication URL and clean up test imports

--- core/authentication/tests/test_home_view.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from django.urls import reverse
-
-class HomeViewTests(TestCase):
-    def test_home_view_renders_correctly(self):
-        response = self.client.get(reverse('authentication:login'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'login/login.html')
-
